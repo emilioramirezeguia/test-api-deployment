c765164ddfb7fe450ea2ce1efa6f9b5f37f97eaf
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-06 10:16:08 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "It's working... it's working!" });
+});
+
+const port = process.env.PORT || 5000;
+server.use(port, () => {
+  console.log(`Server is running at port ${port}`);
+});

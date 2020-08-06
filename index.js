f69dce16687fe8cf0819a3c57723a4788409086a
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-06 10:18:22 -0700

    Update port settings

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ server.get("/", (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-server.use(port, () => {
+server.listen(port, () => {
   console.log(`Server is running at port ${port}`);
 });
